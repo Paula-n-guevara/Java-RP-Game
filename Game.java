3f8ee0d24f0400292894e26a9ad996cf97ee7c7c
Paula Nicole Guevara <123211853+solxsoleil@users.noreply.github.com>
2023-06-06 05:05:11 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,78 @@
+package com.Soleil;
+
+import java.util.Scanner;
+
+public class Game {
+
+
+    private Player human;
+    private Player cpu;
+
+      public Game(String characterChosen, int stats) {
+
+          this.human = human;
+          this.cpu = cpu;
+          }
+
+     void play (){
+     System.out.println("what avatar do you want? select 1, 2, or 3 if available");
+         Scanner input = new Scanner(System.in);
+         System.out.println("Please choose your character") ;
+         String characterChosen = input.nextLine();
+         // --e.g. output--> wizard (1), archer (2), fighter (3)
+
+
+             System.out.println("All the player's available avatars");
+
+
+
+         Scanner input1 = new Scanner(System.in);
+         System.out.println( "Cool what action do you want <archer> to take? Select 1,2, or 3");
+         System.out.println("All of <archer>'s moves");
+         ///--e.g. output--> Shoot Arrow (1), Poison Arrow (2), Dodge (3)
+         input1 = user input // 2 String moveChosen = input.nextLine();
+
+
+     *
+     *   input2 = usr input // 1
+     *   get avatar input1 and perform move input2 on computer's avatar
+     *	 print outcome of move (e.g. damage done)
+     *   computer's turn
+     *	 repeat until either copmuter or human have no more avatar's left alive
+
+}
+}
+    public Game (int y, int x ){
+        int human = player(i);
+        int computer = player(i);
+        int rounds = r;
+
+    }
+
+    public class  get.Play() {
+        System.out.println("What avatar do you want?");
+        for (int i = 0; i < nums.size(); i++)
+        System.out.println(nums.get(i)); //This prints out all of
+        //the available characters that the player still has
+        System.out.println("");
+        System.out.println("");
+
+        Decrease hp(){ //method that decreases the hp
+        avatar.HP(-50);
+        return hp;
+
+        }
+
+        }
+        void attack { //This piece of code should work in order to decreas ethe HP of the avatar by a specific number
+        if(attack = "sword"){
+
+        HP.avatar
+
+        }
+
+
+
+
+        }
+}
